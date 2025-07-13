5e4e13844b831d0d6e1f7a85d74473c2e752d941
TraXannh13 <kyleduong13@gmail.com>
2025-07-13 06:04:06 -0600

    trying to connect supabase

--- quoteCollectorApp/src/App.jsx
+++ quoteCollectorApp/src/App.jsx
@@ -1,6 +1,39 @@
 import './App.css'
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  async function getUsers() {
+    const { data, error } = await supabase
+      .from("testusers")
+      .select()
+    await console.log("Fetched users:", data);
+
+    if (error) {
+      console.error("Error fetching users:", error);
+    } else {
+      setUsers(data);
+    }
+  }
+
+  function getUserOption() {
+    console.log("Users:", users);
+    return users.map((user) => (
+      <option key={user.id} value={user.id}>
+        {user.username}
+      </option>
+    ));
+  }
 
   return (
     <>
@@ -11,11 +44,20 @@ function App() {
           <label htmlFor="quote" className="spanCol2">Enter a Quote:</label>
           <input type="textarea" id="quote" className="spanCol2" name="quote" placeholder="Type your quote here..." required />
           <section>
-            <label htmlFor="author">Author:</label>
-            <input type="text" id="author" name="author" placeholder="Author's name" required />
+            <label htmlFor="author">Who said it:</label>
+            <select id="author" name="author" required>
+              <option value="">Select an author</option>
+              {getUserOption()}
+              {/* <option value="">Select an author</option>
+              {users.map((user) => (
+                <option key={user.id} value={user.id}>
+                {user.username}
+                </option>
+                ))} */}
+            </select>
           </section>
           <section>
-            <label htmlFor="author">Author:</label>
+            <label htmlFor="author">Whose recording it:</label>
             <input type="text" id="author" name="author" placeholder="Author's name" required />
           </section>
           <button type="submit">Submit Quote</button>
