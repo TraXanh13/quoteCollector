531e63c4e31d07bdbf27d41ab126c4ef25971e14
TraXannh13 <kyleduong13@gmail.com>
2025-07-15 19:03:58 -0600

    App is coming along nicely. Including tailwind for easier/faster css

--- quoteCollectorApp/src/App.jsx
+++ quoteCollectorApp/src/App.jsx
@@ -1,68 +1,80 @@
-import './App.css'
 import { useEffect, useState } from "react";
 import { supabase } from "./supabaseClient";
 
+// Importing the Header component
+import Header from "./components/header";
+
 function App() {
-  const [users, setUsers] = useState([]);
+	const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    getUsers();
-  }, []);
+	useEffect(() => {
+		getUsers();
+	}, []);
 
-  async function getUsers() {
-    const { data, error } = await supabase
-      .from("testusers")
-      .select("*")
+	async function getUsers() {
+		const { data, error } = await supabase
+			.from("users")
+			.select("*")
+			.order("first_name", { ascending: true });
 
-    if (error) {
-      console.error("Error fetching users:", error);
-    } else {
-      setUsers(data);
-    }
-    
-    if(!data || data.length === 0) {
-      console.warn("No users found in the database.");
-    }
-  }
+		if (error) {
+			console.error("Error fetching users:", error);
+		} else {
+			setUsers(data);
+		}
 
-  function getUserOption() {
-    return users.map((user) => (
-      <option key={user.id} value={user.id}>
-        {user.username}
-      </option>
-    ));
-  }
+		if (!data || data.length === 0) {
+			console.warn("No users found in the database.");
+		}
+	}
 
-  return (
-    <>
-      <div>
-        <h1>Welcome to Quote Collector!</h1>
-        <p>Collect and share your favorite quotes!</p>
-        <form>
-          <label htmlFor="quote" className="spanCol2">Enter a Quote:</label>
-          <input type="textarea" id="quote" className="spanCol2" name="quote" placeholder="Type your quote here..." required />
-          <section>
-            <label htmlFor="author">Who said it:</label>
-            <select id="author" name="author" required>
-              <option value="">Select an author</option>
-              {getUserOption()}
-              {/* <option value="">Select an author</option>
-              {users.map((user) => (
-                <option key={user.id} value={user.id}>
-                {user.username}
-                </option>
-                ))} */}
-            </select>
-          </section>
-          <section>
-            <label htmlFor="author">Whose recording it:</label>
-            <input type="text" id="author" name="author" placeholder="Author's name" required />
-          </section>
-          <button type="submit">Submit Quote</button>
-        </form>
-      </div>
-    </>
-  )
+	function getUserOption() {
+		return users.map((user) => (
+			<option key={user.id} value={user.id}>
+				{user.first_name} {user.last_name}
+			</option>
+		));
+	}
+
+	const USER_OPTIONS = getUserOption();
+
+	return (
+		<>
+			<Header />
+			<div className="w-screen p-2 text-center my-0 mx-auto">
+				<h1>Welcome to Quote Collector!</h1>
+				<p>Collect and share your favorite quotes!</p>
+				<form className="grid grid-cols-2 gap-4 m-auto max-w-2xl p-4 border-2 border-gray-300 rounded-lg shadow-md">
+					<label htmlFor="quote" className="col-span-2">
+						Enter a Quote:
+					</label>
+					<textarea
+						id="quote"
+						className="col-span-2"
+						name="quote"
+						placeholder="Type your quote here..."
+						required
+					/>
+					<section>
+						<label htmlFor="author">Who said it:</label>
+						<select id="author" name="author" required>
+							<option value="">Select an author</option>
+							{USER_OPTIONS}
+						</select>
+					</section>
+					<section>
+						<label htmlFor="recorder">Whose recording it:</label>
+						<select id="recorder" name="recorder" required>
+							<option value="">Select a recorder</option>
+							<option value="Anonymous">Anonymous</option>
+							{USER_OPTIONS}
+						</select>
+					</section>
+					<button type="submit">Submit Quote</button>
+				</form>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ quoteCollectorApp/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = (props) => {
+	return (
+		<header className="top-0">
+			<p>Quote Collector</p>
+		</header>
+	);
+};
+
+export default Header;
